109739a4008674dbe42b752c3ac7bdec83eba385
Anička Pokorná <anickapokorna06@seznam.cz>
2026-08-13 23:21:55 +0200

    fix: remove redundant GZC watermark from CaseStudiesPage for cleaner design

--- components/pages/case-studies/CaseStudiesPage.tsx
+++ components/pages/case-studies/CaseStudiesPage.tsx
@@ -85,12 +85,6 @@ export default function CaseStudiesPage({
             {chrome.subtitle}
           </motion.p>
         </div>
-        <div
-          aria-hidden
-          className="pointer-events-none absolute -right-8 top-1/2 -translate-y-1/2 select-none text-[18vw] font-black leading-none text-black/5"
-        >
-          GZC
-        </div>
       </section>
 
       {/* ── CASE STUDIES ── */}
